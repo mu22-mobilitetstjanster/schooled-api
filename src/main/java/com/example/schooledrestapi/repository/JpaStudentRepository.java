3dbf9373eff9bd62b0be18e2e26d53ad9dba88ad
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2023-02-16 15:15:32 +0100

    Delete src directory

--- src/main/java/com/example/schooledrestapi/repository/JpaStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.schooledrestapi.repository;
-
-import com.example.schooledrestapi.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("studentRepository")
-public interface JpaStudentRepository extends JpaRepository<Student, Long> {
-}
