5d91ec5a10d7660924565421400b94793673b65e
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-02-19 20:06:40 +0100

    seperated course api

--- schooled-management-api/src/main/java/com/example/schooledmanagementapi/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.schooledmanagementapi.repository;
-
-import com.example.schooledmanagementapi.model.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-}
