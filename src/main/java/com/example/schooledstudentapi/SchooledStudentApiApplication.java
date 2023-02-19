906ddfdc8f23284083010ae8b0c296f661a96209
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-02-19 22:20:26 +0100

    added line runner

--- src/main/java/com/example/schooledstudentapi/SchooledStudentApiApplication.java
+++ src/main/java/com/example/schooledstudentapi/SchooledStudentApiApplication.java
@@ -1,7 +1,12 @@
 package com.example.schooledstudentapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.schooledstudentapi.model.Student;
+import com.example.schooledstudentapi.repository.JpaStudentRepository;
 
 @SpringBootApplication
 public class SchooledStudentApiApplication {
@@ -10,4 +15,13 @@ public class SchooledStudentApiApplication {
     SpringApplication.run(SchooledStudentApiApplication.class, args);
   }
 
+  @Bean
+  public CommandLineRunner run(JpaStudentRepository studentRepository) {
+    return args -> {
+      Student student = new Student();
+      student.setName("Bertil");
+
+      studentRepository.save(student);
+    };
+  }
 }
--- src/main/java/com/example/schooledstudentapi/controller/StudentController.java
+++ src/main/java/com/example/schooledstudentapi/controller/StudentController.java
@@ -1,15 +1,25 @@
 package com.example.schooledstudentapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.schooledstudentapi.model.Student;
 import com.example.schooledstudentapi.service.StudentService;
 import com.example.schooledstudentapi.ws.StudentStateSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
+@RequestMapping("/student/*")
 public class StudentController {
 
   @Autowired
@@ -18,53 +28,57 @@ public class StudentController {
   @Autowired
   private StudentService studentService;
 
-  @GetMapping("student/{studentId}")
+  @GetMapping("{studentId}")
   public ResponseEntity<Student> getStudentById(@PathVariable long studentId) {
     return ResponseEntity.ok(studentService.get(studentId));
   }
 
-  @GetMapping("student")
+  @GetMapping
   public ResponseEntity<List<Student>> getAllStudents() {
     return ResponseEntity.ok(studentService.getAll());
   }
 
-  @PostMapping("student")
-  @PatchMapping("student")
-  @PutMapping("student")
+  @PostMapping
+  @PatchMapping
+  @PutMapping
   public ResponseEntity<List<Student>> addStudent(@RequestBody Student student) {
     studentService.save(student);
     studentStateSocketHandler.broadcast("new-student", student.getName() + " was created");
     return getAllStudents();
   }
 
-  @DeleteMapping("student")
+  @DeleteMapping
   public ResponseEntity<List<Student>> deleteStudent(@RequestBody Student student) {
     studentService.delete(student.getId());
     return getAllStudents();
   }
 
-  @DeleteMapping("student/{studentId}")
+  @DeleteMapping("{studentId}")
   public ResponseEntity<List<Student>> deleteStudent(@PathVariable long studentId) {
     studentService.delete(studentId);
     return getAllStudents();
   }
 
-  @PatchMapping("student/online/{state}/{studentId}")
+  @PatchMapping("online/{state}/{studentId}")
   public void setOnlineState(@PathVariable String state, @PathVariable long studentId) {
     Student newStudent = studentService.get(studentId);
     Student oldStudent = newStudent.clone();
 
-    switch(state) {
-      case "online": newStudent.setOnline(true); break;
-      case "offline": newStudent.setOnline(false); break;
-      default: throw new IllegalStateException(state + " was illdefined");
+    switch (state) {
+      case "online":
+        newStudent.setOnline(true);
+        break;
+      case "offline":
+        newStudent.setOnline(false);
+        break;
+      default:
+        throw new IllegalStateException(state + " was illdefined");
     }
-
 
     studentService.save(newStudent);
 
     // broadcast student changes
-    if(newStudent.isOnline()) {
+    if (newStudent.isOnline()) {
       studentStateSocketHandler.broadcast("online", oldStudent, newStudent);
     } else {
       studentStateSocketHandler.broadcast("offline", oldStudent, newStudent);
