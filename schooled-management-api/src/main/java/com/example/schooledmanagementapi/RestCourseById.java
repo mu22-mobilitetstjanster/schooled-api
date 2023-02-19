68198187b46f40ba2965ba1516ebf66cbeac462a
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-02-19 20:08:35 +0100

    flatted config

--- schooled-management-api/src/main/java/com/example/schooledmanagementapi/RestCourseById.java
+++
@@ -1,8 +0,0 @@
-package com.example.schooledmanagementapi;
-
-import com.example.schooledmanagementapi.model.CourseDetails;
-
-@FunctionalInterface
-public interface RestCourseById {
-  CourseDetails find(Long id);
-}
---
+++ src/test/java/com/example/schooledconfigserver/SchooledConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schooledconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooledConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
